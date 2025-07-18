69840029b6a3c496a4dd94ffb7565437807afc95
Rahul Chowdhury <rahul.chowdhury@kellton.org>
2025-07-18 15:55:02 +0530

    18/07/2025 changes

--- src/A.java
+++ src/A.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class A {
+    public static void main(String ...args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n = scan.nextInt();
+        if(n % 2 ==0){
+            System.out.println(n+ " is Even no");
+        }
+        else {
+            System.out.println(n+ " is Odd no");
+        }
+        scan.close();
+    }
 }
