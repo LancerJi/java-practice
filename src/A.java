0f0ef7fd9968318307de5225b92fd7953058a503
Rahul Chowdhury <rahul.chowdhury@kellton.org>
2025-07-31 18:30:49 +0530

    commit on 31/07-2025

--- src/A.java
+++ src/A.java
@@ -1,8 +1,10 @@
-import java.util.Scanner;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class A {
-    public static void main(String ...args){
+    public static void main(String ...args) {
 //        Scanner scan = new Scanner(System.in);
 //        System.out.println("Enter number: ");
 //        int n = scan.nextInt();
@@ -14,18 +16,60 @@ public class A {
 //        }
 //        scan.close();
 //    }
-    Scanner scan = new Scanner(System.in);
-        System.out.println("enter no: ");
+//    Scanner scan = new Scanner(System.in);
+//        System.out.println("enter no: ");
+//
+//    String result = IntStream.of()
+//                            .mapToObj(n-> {
+//                                if (n % 2 == 0) {
+//                                    return n + " is even no";
+//                                }else{
+//                                    return n+ " is odd no";
+//                                }
+//                            }).findFirst()
+//                            .orElse("Invalid");
+//                            System.out.println(result);
+//        }
 
-    String result = IntStream.of()
-                            .mapToObj(n-> {
-                                if (n % 2 == 0) {
-                                    return n + " is even no";
-                                }else{
-                                    return n+ " is odd no";
-                                }
-                            }).findFirst()
-                            .orElse("Invalid");
-                            System.out.println(result);
-        }
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("enter your name to check: ");
+//        String inputName = scan.nextLine();
+////        String [] names = {scan};//given the names to filter
+//
+//        Predicate <String> p = name->name.startsWith("M");//using the predicate function to return boolean value and starts with A
+////            for(String name:names){//used for loop for iterating over names
+//                if(p.test(inputName)){//validating with test method that name start with A or not
+//                    System.out.println(inputName+ " name starts with M");// printing the name if found
+//                }
+//                else {
+//                    System.out.println(inputName+ " this name doesnt start with M");
+//                }
+//                scan.close();
+
+//        List<String> ls = Arrays.asList("Rahul", "Ram", "Shym", "Krshn", "Sky","Equation");
+//
+//        List<String> namesWithVowels = ls.stream()
+//                .filter(w->w.toUpperCase())
+//                .collect(Collectors.toList());
+//        System.out.println(namesWithVowels);
+
+//        String s= "I am learning Streams API in Java";
+
+//        String ans = Arrays.stream(s.split(" "))
+//                    .max(Comparator.comparing(String::length))
+//                    .get();
+//                    System.out.print(ans);
+
+//        String s = "I am learning Streams API in Java";
+//        String ans = Arrays.stream(s.split(" "))
+//                .max(Comparator.comparing(String::length))
+//                .get();
+//        System.out.println(ans);
+
+        String s = "dabcadefg";
+
+
+
+
+    }
 }
